8bded2432cc80f87b431f8811635b3f626277a6a
NIMIJITH N J <154909952+NIMIJITHNJ@users.noreply.github.com>
2024-04-07 17:09:50 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('', views.home, name='home'),
+    path('registration/', views.register_user, name='registration'),
+]
+
